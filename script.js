8abf88b9e1322a1fa7357d912f472676ca1e14e2
mrwks11 <w.k.sinke@gmail.com>
2020-10-12 17:24:53 +0200

    Added player names & scores to UI

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-const gameBoard = document.querySelector('.game-board');
+// Global variables
+const gameBoard = document.getElementById('game-board');
 const allCells = document.querySelectorAll('[data-cell]');
-
 const crossPlayer = 'X';
 const circlePlayer = 'O';
-let currentPlayer = crossPlayer;
+currentPlayer = crossPlayer;
+gameRound = 0;
+let currentBoard = ['', '', '', '', '', '', '', ''];
 
-let currentBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+// Player names
+const playerOneName = document.getElementById('playerOne');
+const playerTwoName = document.getElementById('playerTwo');
 
+// Player Scores
+const playerOneScore = document.getElementById('playerOneScore');
+const playerTwoScore = document.getElementById('playerTwoScore');
+
+// Game buttons
+const startGame = document.getElementById('startNewGame');
+startGame.addEventListener('click', (e) => {
+  startNewGame();
+})
+
+// Start a new game
+function startNewGame() {
+  const div = document.querySelector('.winner-screen');
+  div.remove(); 
+  for (let i = 0; i < allCells.length; i++) {
+    allCells[i].classList.remove('X', 'O');
+    allCells[i].textContent = '';
+  }
+  currentBoard = ['', '', '', '', '', '', '', ''];
+  currentPlayer = crossPlayer;
+  gameRound = 0;
+}
+
+// Winning combinations
 const winningCombinations = [
   // Horizontal
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
   // Vertical
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
   // Diagonal
-  [0, 4, 8],
-  [2, 4, 6],
+  [0, 4, 8], [2, 4, 6],
 ];
 
-// Event: cell clicked on game board
+// Event: when cell is clicked on gameBoard
 gameBoard.addEventListener('click', (e) => {
   clickAction(e.target);
 });
@@ -41,13 +64,14 @@ function clickAction(target) {
   }
 }
 
-// Update game board
+// Update gameBoard
 function updateGameBoard(target) {
   // Update HTML & UI
   target.classList.add(`${currentPlayer}`);
   target.textContent = `${currentPlayer}`;
+  gameRound++;
 
-  // Update current board array
+  // Update currentBoard array
   for (let i = 0; i < allCells.length; i++) {
     if (allCells[i].textContent === 'X') {
       currentBoard[i] = 'X';
@@ -61,17 +85,22 @@ function updateGameBoard(target) {
 
 // Check if there is a winner
 function checkWinner() {
-  for (let i = 0; i < winningCombinations.length; i++) {
-    if (
-      (currentBoard[winningCombinations[i][0]] === 'X' &&
-        currentBoard[winningCombinations[i][1]] === 'X' &&
-        currentBoard[winningCombinations[i][2]] === 'X') ||
-      (currentBoard[winningCombinations[i][0]] === 'O' &&
-        currentBoard[winningCombinations[i][1]] === 'O' &&
-        currentBoard[winningCombinations[i][2]] === 'O')
-    ) {
-      showWinnerScreen();
-    } 
+  if (gameRound < 9) {
+    for (let i = 0; i < winningCombinations.length; i++) {
+      if (
+        (currentBoard[winningCombinations[i][0]] === 'X' &&
+          currentBoard[winningCombinations[i][1]] === 'X' &&
+          currentBoard[winningCombinations[i][2]] === 'X') ||
+        (currentBoard[winningCombinations[i][0]] === 'O' &&
+          currentBoard[winningCombinations[i][1]] === 'O' &&
+          currentBoard[winningCombinations[i][2]] === 'O')
+      ) {
+        let endGame = 'winner';
+        showWinnerScreen(endGame);
+      } 
+    }
+  } else {
+    showWinnerScreen();
   }
 }
 
@@ -83,28 +112,24 @@ function changePlayer() {
 }
 
 // Display winner screen
-function showWinnerScreen() {
+function showWinnerScreen(endGame) {
   const div = document.createElement('div');
-  const body = document.querySelector('body');
-  const winnerScreen = body.insertBefore(div, gameBoard);
+  const container = document.querySelector('.container');
+  const winnerScreen = container.insertBefore(div, gameBoard);
   winnerScreen.classList.add('winner-screen');
+  
+  if (endGame === 'winner') {
   winnerScreen.innerHTML = `
         "${currentPlayer}" is the winner!
-        <button class="btn">Play a new game</button>
+        <button class="btn-restart">Play a new game</button>
         `;
-  // Event: click button, start new game
-  button = document.querySelector('.btn');
-  button.addEventListener('click', (e) => startNewGame());
-}
-
-// Start a new game
-function startNewGame() {
-  const div = document.querySelector('.winner-screen');
-  div.remove();
-  for (let i = 0; i < allCells.length; i++) {
-    allCells[i].classList.remove('X', 'O');
-    allCells[i].textContent = '';
+  } else {
+  winnerScreen.innerHTML = `
+        It is a draw!
+        <button class="btn-restart">Play a new game</button>
+        `;
   }
-  currentBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  currentPlayer = crossPlayer;
+  // Event: click button, start new game
+  let button = document.querySelector('.btn-restart');
+  button.addEventListener('click', (e) => startNewGame());
 }
